46eb7a4d7842def8139737306c092284704f0a84
Zach <zach@ganderworks.com>
2025-12-13 14:56:03 -0800

    vlts

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -36,6 +36,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.ui.mini-starter" },
     { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
     -- import/override with your plugins (must be last)
     { import = "plugins" },
   },
--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -63,10 +63,6 @@ return {
     "BurntSushi/ripgrep",
   },
 
-  -- for typescript, LazyVim also includes extra specs to properly setup lspconfig,
-  -- treesitter, mason and typescript.nvim. So instead of the above, you can use:
-  -- { import = "lazyvim.plugins.extras.lang.typescript" },
-
   -- add more treesitter parsers
   {
     "nvim-treesitter/nvim-treesitter",
